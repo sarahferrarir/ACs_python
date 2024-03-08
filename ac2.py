12172ce86a24101825a3bb6e7e4efe612bafbe7a
sarahferrarir <143027206+sarahferrarir@users.noreply.github.com>
2024-03-08 09:32:19 -0300

    Update ac2.py

--- ac2.py
+++ ac2.py
@@ -1,6 +1,10 @@
 #AC2 - EXERCÍCIO 1 - revisite a AC1
-#AC1: exercício 1
-
+"""
+Exercício 1: revisite a AC1
+Desenvolva duas funções em Python:
+1)eq_seg_grau(a, b, c), que recebe três valores reais e retorna as raízes de uma equação de segundo grau no formato ax² + bx + c = 0,
+supondo as raízes sempre reais;
+"""
 def eq_seg_grau(a, b, c):
     delta = b * b - (4 * a * c)
     raiz1 = (-b + (delta ** 0.5) ) / (2*a)
@@ -15,8 +19,10 @@ c = float( input("Informe o coeficiente c: ") )
 raizes = eq_seg_grau(a, b, c)
 print(f"As raizes da equação são: {raizes[0]} e {raizes[1]}")
 
-#AC1: exercício 2
-
+"""
+Desenvolva duas funções em Python:
+2)bissexto(ano), que recebe um valor inteiro e retorna um valor booleano, informando se o ano é bissexto ou não.
+"""
 def bissexto(ano):
     return(ano % 4 ==0 and ano % 100 != 0 or ano % 400 == 0)
 
@@ -25,7 +31,13 @@ ano = float(input("Informe o ano: "))
 print(bissexto(ano))
 
 #AC2 - EXERCÍCIO 2 - salário
-
+"""
+Desenvolva uma função em Python cujo nome é calcula_salario. Essa função recebe dois parâmetros posicionais reais,
+valor_hora e num_horas, que correspondem ao valor do salário por hora e o número de horas trabalhadas no mês, respectivamente.
+Além disso, a função tem um parâmetro-chave irpf, que calcula o imposto de renda a ser deduzido, cujo valor padrão é 0.275.
+A função retorna o salário líquido de um funcionário, calculado como o produto do valor por hora pelo número de horas,
+reduzido o percentual do imposto de renda dado.
+"""
 def calcula_salario(valor_hora, num_horas, irpf=0.275):
     return(valor_hora * num_horas - (valor_hora * num_horas * irpf))
 
